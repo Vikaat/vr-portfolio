04ca87cd426ff3a2ab35126a21de81ccf8a81d72
Vikaat Ravi <vikaat08@gmail.com>
2024-08-09 11:51:11 -0700

    Animations

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multibio", {
+    strings:["Developer", "Founder", "Cloud Practitioner","Data Engineer", "Student"], 
+    typeSpeed: 150,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
